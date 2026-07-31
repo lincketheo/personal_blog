9cf8fb595932aeb9e54161ad347797d19fac03ba
Theo Lincke <lincketheo@gmail.com>
2026-07-31 16:33:10 -0400

    fixes twitter fa import

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import App from "./App.vue";
 import router from "./router";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedin, faXTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faGithub, faLinkedin, faBars, faXmark);
+library.add(faGithub, faLinkedin, faXTwitter, faBars, faXmark);
 
 const app = createApp(App).component("font-awesome-icon", FontAwesomeIcon);
 app.use(router);
